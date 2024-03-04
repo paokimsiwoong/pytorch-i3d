2bd573f3d6766d6862ef37d7005c065a63fbc58a
KimSiwoong <pao.kim.si.woong@gmail.com>
2024-03-04 17:24:44 +0900

    [feat] test 코드

--- charades_dataset_full.py
+++ charades_dataset_full.py
@@ -5,92 +5,113 @@ from torch.utils.data.dataloader import default_collate
 import numpy as np
 import json
 import csv
-import h5py
+
+# import h5py
 
 import os
 import os.path
 
 import cv2
 
+
 def video_to_tensor(pic):
     """Convert a ``numpy.ndarray`` to tensor.
     Converts a numpy.ndarray (T x H x W x C)
     to a torch.FloatTensor of shape (C x T x H x W)
-    
+
     Args:
          pic (numpy.ndarray): Video to be converted to tensor.
     Returns:
          Tensor: Converted video.
     """
-    return torch.from_numpy(pic.transpose([3,0,1,2]))
+    return torch.from_numpy(pic.transpose([3, 0, 1, 2]))
 
 
 def load_rgb_frames(image_dir, vid, start, num):
-  frames = []
-  for i in range(start, start+num):
-    img = cv2.imread(os.path.join(image_dir, vid, vid+'-'+str(i).zfill(6)+'.jpg'))[:, :, [2, 1, 0]]
-    w,h,c = img.shape
-    if w < 226 or h < 226:
-        d = 226.-min(w,h)
-        sc = 1+d/min(w,h)
-        img = cv2.resize(img,dsize=(0,0),fx=sc,fy=sc)
-    img = (img/255.)*2 - 1
-    frames.append(img)
-  return np.asarray(frames, dtype=np.float32)
+    frames = []
+    for i in range(start, start + num):
+        img = cv2.imread(os.path.join(image_dir, vid, vid + "-" + str(i).zfill(6) + ".jpg"))[:, :, [2, 1, 0]]
+        w, h, c = img.shape
+        if w < 226 or h < 226:
+            d = 226.0 - min(w, h)
+            sc = 1 + d / min(w, h)
+            img = cv2.resize(img, dsize=(0, 0), fx=sc, fy=sc)
+        img = (img / 255.0) * 2 - 1
+        frames.append(img)
+    return np.asarray(frames, dtype=np.float32)
+
 
 def load_flow_frames(image_dir, vid, start, num):
-  frames = []
-  for i in range(start, start+num):
-    imgx = cv2.imread(os.path.join(image_dir, vid, vid+'-'+str(i).zfill(6)+'x.jpg'), cv2.IMREAD_GRAYSCALE)
-    imgy = cv2.imread(os.path.join(image_dir, vid, vid+'-'+str(i).zfill(6)+'y.jpg'), cv2.IMREAD_GRAYSCALE)
-    
-    w,h = imgx.shape
-    if w < 224 or h < 224:
-        d = 224.-min(w,h)
-        sc = 1+d/min(w,h)
-        imgx = cv2.resize(imgx,dsize=(0,0),fx=sc,fy=sc)
-        imgy = cv2.resize(imgy,dsize=(0,0),fx=sc,fy=sc)
-        
-    imgx = (imgx/255.)*2 - 1
-    imgy = (imgy/255.)*2 - 1
-    img = np.asarray([imgx, imgy]).transpose([1,2,0])
-    frames.append(img)
-  return np.asarray(frames, dtype=np.float32)
+    frames = []
+    for i in range(start, start + num):
+        imgx = cv2.imread(
+            os.path.join(image_dir, vid, vid + "-" + str(i).zfill(6) + "x.jpg"), cv2.IMREAD_GRAYSCALE
+        )
+        imgy = cv2.imread(
+            os.path.join(image_dir, vid, vid + "-" + str(i).zfill(6) + "y.jpg"), cv2.IMREAD_GRAYSCALE
+        )
+
+        w, h = imgx.shape
+        if w < 224 or h < 224:
+            d = 224.0 - min(w, h)
+            sc = 1 + d / min(w, h)
+            imgx = cv2.resize(imgx, dsize=(0, 0), fx=sc, fy=sc)
+            imgy = cv2.resize(imgy, dsize=(0, 0), fx=sc, fy=sc)
+
+        imgx = (imgx / 255.0) * 2 - 1
+        imgy = (imgy / 255.0) * 2 - 1
+        img = np.asarray([imgx, imgy]).transpose([1, 2, 0])
+        frames.append(img)
+    return np.asarray(frames, dtype=np.float32)
+
+
+def load_rgb_frames_npy(image_dir, vid, start, num):
+
+    return np.load(os.path.join(image_dir, vid + "_rgb.npy"))
+
+
+def load_flow_frames_npy(image_dir, vid, start, num):
+
+    return np.load(os.path.join(image_dir, vid + "_flow.npy"))
 
 
 def make_dataset(split_file, split, root, mode, num_classes=157):
     dataset = []
-    with open(split_file, 'r') as f:
+    with open(split_file, "r") as f:
         data = json.load(f)
+
+    print(data.keys())
 
     i = 0
     for vid in data.keys():
-        if data[vid]['subset'] != split:
+        print(f"==>> vid: {vid}")
+        if data[vid]["subset"] != split:
             continue
 
         if not os.path.exists(os.path.join(root, vid)):
             continue
         num_frames = len(os.listdir(os.path.join(root, vid)))
-        if mode == 'flow':
-            num_frames = num_frames//2
-            
-        label = np.zeros((num_classes,num_frames), np.float32)
+        if mode == "flow":
+            num_frames = num_frames // 2
 
-        fps = num_frames/data[vid]['duration']
-        for ann in data[vid]['actions']:
-            for fr in range(0,num_frames,1):
-                if fr/fps > ann[1] and fr/fps < ann[2]:
-                    label[ann[0], fr] = 1 # binary classification
-        dataset.append((vid, label, data[vid]['duration'], num_frames))
+        label = np.zeros((num_classes, num_frames), np.float32)
+        print(f"==>> label.shape: {label.shape}")
+
+        fps = num_frames / data[vid]["duration"]
+        for ann in data[vid]["actions"]:
+            for fr in range(0, num_frames, 1):
+                if fr / fps > ann[1] and fr / fps < ann[2]:
+                    label[ann[0], fr] = 1  # binary classification
+        dataset.append((vid, label, data[vid]["duration"], num_frames))
         i += 1
-    
+
     return dataset
 
 
 class Charades(data_utl.Dataset):
 
-    def __init__(self, split_file, split, root, mode, transforms=None, save_dir='', num=0):
-        
+    def __init__(self, split_file, split, root, mode, transforms=None, save_dir="", num=0):
+
         self.data = make_dataset(split_file, split, root, mode)
         self.split_file = split_file
         self.transforms = transforms
@@ -107,15 +128,96 @@ class Charades(data_utl.Dataset):
             tuple: (image, target) where target is class_index of the target class.
         """
         vid, label, dur, nf = self.data[index]
-        if os.path.exists(os.path.join(self.save_dir, vid+'.npy')):
+        if os.path.exists(os.path.join(self.save_dir, vid + ".npy")):
             return 0, 0, vid
 
-        if self.mode == 'rgb':
+        if self.mode == "rgb":
             imgs = load_rgb_frames(self.root, vid, 1, nf)
         else:
             imgs = load_flow_frames(self.root, vid, 1, nf)
 
         imgs = self.transforms(imgs)
+
+        return video_to_tensor(imgs), torch.from_numpy(label), vid
+
+    def __len__(self):
+        return len(self.data)
+
+
+def make_dataset_npy(split_file, split, root, mode, num_classes=157):
+    dataset = []
+    with open(split_file, "r") as f:
+        data = json.load(f)
+
+    print(data.keys())
+
+    i = 0
+    for vid in data.keys():
+        print(f"==>> vid: {vid}")
+        if data[vid]["subset"] != split:
+            continue
+
+        # if not os.path.exists(os.path.join(root, vid)):
+        #     continue
+        # num_frames = len(os.listdir(os.path.join(root, vid)))
+        # if mode == "flow":
+        #     num_frames = num_frames // 2
+
+        num_frames = 7900
+        print(f"==>> num_frames: {num_frames}")
+
+        label = np.zeros((num_classes, num_frames), np.float32)
+        print(f"==>> label.shape: {label.shape}")
+
+        fps = num_frames / data[vid]["duration"]
+        for ann in data[vid]["actions"]:
+            for fr in range(0, num_frames, 1):
+                if fr / fps > ann[1] and fr / fps < ann[2]:
+                    label[ann[0], fr] = 1  # binary classification
+        dataset.append((vid, label, data[vid]["duration"], num_frames))
+        i += 1
+
+    return dataset
+
+
+class Charades_npy(data_utl.Dataset):
+
+    def __init__(self, split_file, split, root, mode, transforms=None, save_dir="", num=0):
+
+        self.data = make_dataset_npy(split_file, split, root, mode)
+        # print(f"==>> self.data: {self.data}")
+        self.split_file = split_file
+        self.transforms = transforms
+        self.mode = mode
+        self.root = root
+        self.save_dir = save_dir
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is class_index of the target class.
+        """
+        vid, label, dur, nf = self.data[index]
+
+        if self.mode == "rgb":
+            imgs = load_rgb_frames_npy(self.root, vid, 1, nf)
+            imgs = np.random.normal(0, 1, (7900, 224, 224, 3))
+            imgs = imgs.astype(np.float32)
+        else:
+            imgs = load_flow_frames_npy(self.root, vid, 1, nf)
+            imgs = np.random.normal(0, 1, (7900, 224, 224, 2))
+            imgs = imgs.astype(np.float32)
+
+        print(f"==>> imgs.shape: {imgs.shape}")
+        imgs = np.squeeze(imgs)
+
+        print(f"==>> imgs.shape: {imgs.shape}")
+
+        imgs = self.transforms(imgs)
+        print(f"==>> imgs.shape: {imgs.shape}")
 
         return video_to_tensor(imgs), torch.from_numpy(label), vid
 
--- extract_features.py
+++ extract_features.py
@@ -1,17 +1,18 @@
 import os
-os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   
+
+# os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 import sys
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-mode', type=str, help='rgb or flow')
-parser.add_argument('-load_model', type=str)
-parser.add_argument('-root', type=str)
-parser.add_argument('-gpu', type=str)
-parser.add_argument('-save_dir', type=str)
+parser.add_argument("-mode", type=str, help="rgb or flow")
+parser.add_argument("-load_model", type=str)
+parser.add_argument("-root", type=str)
+parser.add_argument("-gpu", type=str)
+parser.add_argument("-save_dir", type=str)
 
 args = parser.parse_args()
-os.environ["CUDA_VISIBLE_DEVICES"]=args.gpu
+# os.environ["CUDA_VISIBLE_DEVICES"] = "cuda"
 
 import torch
 import torch.nn as nn
@@ -29,62 +30,97 @@ import numpy as np
 
 from pytorch_i3d import InceptionI3d
 
-from charades_dataset_full import Charades as Dataset
+# from charades_dataset_full import Charades as Dataset
+from charades_dataset_full import Charades_npy as Dataset
+
+from tqdm import tqdm
 
 
-def run(max_steps=64e3, mode='rgb', root='/ssd2/charades/Charades_v1_rgb', split='charades/charades.json', batch_size=1, load_model='', save_dir=''):
+def run(
+    max_steps=64e3,
+    mode="rgb",
+    root="/data/ephemeral/home/pytorch-i3d/data/rgbs",
+    split="/data/ephemeral/home/pytorch-i3d/data/sample.json",
+    batch_size=1,
+    load_model="/data/ephemeral/home/pytorch-i3d/models/rgb_charades.pt",
+    save_dir="/data/ephemeral/home/pytorch-i3d/sample_feat",
+):
     # setup dataset
     test_transforms = transforms.Compose([videotransforms.CenterCrop(224)])
 
-    dataset = Dataset(split, 'training', root, mode, test_transforms, num=-1, save_dir=save_dir)
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=8, pin_memory=True)
+    dataset = Dataset(split, "training", root, mode, test_transforms, num=-1, save_dir=save_dir)
+    dataloader = torch.utils.data.DataLoader(
+        dataset, batch_size=batch_size, shuffle=True, num_workers=0, pin_memory=True
+    )
 
-    val_dataset = Dataset(split, 'testing', root, mode, test_transforms, num=-1, save_dir=save_dir)
-    val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=8, pin_memory=True)    
+    # val_dataset = Dataset(split, "testing", root, mode, test_transforms, num=-1, save_dir=save_dir)
+    # val_dataloader = torch.utils.data.DataLoader(
+    #     val_dataset, batch_size=batch_size, shuffle=True, num_workers=8, pin_memory=True
+    # )
+    val_dataset = None
+    val_dataloader = None
 
-    dataloaders = {'train': dataloader, 'val': val_dataloader}
-    datasets = {'train': dataset, 'val': val_dataset}
+    dataloaders = {"train": dataloader, "val": val_dataloader}
+    datasets = {"train": dataset, "val": val_dataset}
 
-    
     # setup the model
-    if mode == 'flow':
+    if mode == "flow":
         i3d = InceptionI3d(400, in_channels=2)
     else:
         i3d = InceptionI3d(400, in_channels=3)
     i3d.replace_logits(157)
     i3d.load_state_dict(torch.load(load_model))
-    i3d.cuda()
+    print(f"==>> torch.cuda.is_available(): {torch.cuda.is_available()}")
+    i3d.to(device="cuda")
 
-    for phase in ['train', 'val']:
+    for phase in ["train"]:
         i3d.train(False)  # Set model to evaluate mode
-                
+
         tot_loss = 0.0
         tot_loc_loss = 0.0
         tot_cls_loss = 0.0
-                    
+
+        print(f"==>> len(dataloaders[phase]): {len(dataloaders[phase])}")
+
         # Iterate over data.
         for data in dataloaders[phase]:
             # get the inputs
             inputs, labels, name = data
-            if os.path.exists(os.path.join(save_dir, name[0]+'.npy')):
+            if os.path.exists(os.path.join(save_dir, name[0] + ".npy")):
                 continue
 
-            b,c,t,h,w = inputs.shape
+            b, c, t, h, w = inputs.shape
             if t > 1600:
                 features = []
-                for start in range(1, t-56, 1600):
-                    end = min(t-1, start+1600+56)
-                    start = max(1, start-48)
-                    ip = Variable(torch.from_numpy(inputs.numpy()[:,:,start:end]).cuda(), volatile=True)
-                    features.append(i3d.extract_features(ip).squeeze(0).permute(1,2,3,0).data.cpu().numpy())
+                for start in range(1, t - 56, 1600):
+                    end = min(t - 1, start + 1600 + 56)
+                    start = max(1, start - 48)
+                    ip = Variable(torch.from_numpy(inputs.numpy()[:, :, start:end]).cuda(), volatile=True)
+                    features.append(
+                        i3d.extract_features(ip).squeeze(0).permute(1, 2, 3, 0).data.cpu().numpy()
+                    )
                 np.save(os.path.join(save_dir, name[0]), np.concatenate(features, axis=0))
             else:
                 # wrap them in Variable
                 inputs = Variable(inputs.cuda(), volatile=True)
                 features = i3d.extract_features(inputs)
-                np.save(os.path.join(save_dir, name[0]), features.squeeze(0).permute(1,2,3,0).data.cpu().numpy())
+                np.save(
+                    os.path.join(save_dir, name[0]),
+                    features.squeeze(0).permute(1, 2, 3, 0).data.cpu().numpy(),
+                )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # need to add argparse
-    run(mode=args.mode, root=args.root, load_model=args.load_model, save_dir=args.save_dir)
+    for i in tqdm(range(2), total=2):
+        if i == 0:
+            run()
+        else:
+            run(
+                mode="flow",
+                root="/data/ephemeral/home/pytorch-i3d/data/flows",
+                split="/data/ephemeral/home/pytorch-i3d/data/sample.json",
+                batch_size=1,
+                load_model="/data/ephemeral/home/pytorch-i3d/models/flow_charades.pt",
+                save_dir="/data/ephemeral/home/pytorch-i3d/sample_feat",
+            )
